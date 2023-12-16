8c8f3e824dda943a72c6ebc24e88848bca5ac9fb
mahdi <mahdimamashli1383@gmail.com>
2023-12-16 17:14:47 +0330

    Update the home page to display products from the database.

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 const Product = require("../models/products")
 exports.getHome = (req,res,next)=>{
     Product.findAll().then(products =>{
+        console.log('prod => ', products);
+        console.log('prodsssss => ', products[0]);
         res.render('index', {
             title : 'HOME',
             path : req.path,
-            products
+            products : products
         })
     })
     
