acb4f53352fe0dddfd379f6bb8402527ca83d9ff
mahdi <mahdimamashli1383@gmail.com>
2023-12-16 16:40:50 +0330

    Changed the path for the add product page from admin to shop.

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require("../models/products");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("admin/add-product", { pageTitle: "Add Product", path: req.path });
+  res.render("shop/add-product", { title: "Add Product", path: req.path });
 };
