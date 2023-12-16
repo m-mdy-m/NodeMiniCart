effe8db5d187054820b01f939aff1ae6ef53c0cc
mahdi <mahdimamashli1383@gmail.com>
2023-12-16 18:48:22 +0330

    Add auto-incrementing primary key to User model

--- models/users.js
+++ models/users.js
@@ -5,9 +5,12 @@ const sequelize = require("../database/database");
 const User = sequelize.define({
   id: {
     typ: Sequelize.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
   },
   name: Sequelize.STRING,
   email: Sequelize.STRING,
 });
 
-module.exports = User
+module.exports = User;
