204529254b741cac3fe11ea91a9179eb94ec96e4
mahdi <mahdimamashli1383@gmail.com>
2023-12-17 00:56:32 +0330

    Change user ID from 2 to 1 in the code.

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
-  User.findByPk(2)
+  User.findByPk(1)
     .then((user) => {
       req.user = user;
       next();
@@ -44,7 +44,7 @@ sequelize
   .sync()
   // .sync({ force: true })
   .then((result) => {
-    return User.findByPk(2);
+    return User.findByPk(1);
   })
   .then((us) => {
     if (!us) {
