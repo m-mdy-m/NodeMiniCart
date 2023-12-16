0e9026cc307d5a1b507aac8e7a5a0f52a19f8e53
mahdi <mahdimamashli1383@gmail.com>
2023-12-16 16:58:10 +0330

    Add product creation functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,3 +3,16 @@ const Product = require("../models/products");
 exports.getAddProduct = (req, res, next) => {
   res.render("shop/add-product", { title: "Add Product", path: req.path });
 };
+exports.postAddProduct = (req,res,next)=>{
+    const title = req.body.title
+    const price = req.body.price
+    Product.create({
+        title,
+        price
+    }).then((result) => {
+        console.log('users created');
+        res.redirect('/')
+    }).catch((err) => {
+        console.log(err);
+    });
+}
--- routes/shop.js
+++ routes/shop.js
@@ -2,5 +2,5 @@ const express = require("express")
 const routes = express.Router()
 const shopControllers = require("../controllers/shop")
 routes.get('/add-products', shopControllers.getAddProduct)
-
+routes.post('/add-products' , shopControllers.postAddProduct)
 module.exports = routes
