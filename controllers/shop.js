459495890db4be7c8f2185bc7e17ec9658bf7bf1
mahdi <mahdimamashli1383@gmail.com>
2023-12-16 17:31:48 +0330

    Add product edit and delete functionality (generated by blackbox.ai )

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,18 +1,24 @@
 const Product = require("../models/products");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("shop/add-product", { title: "Add Product", path: req.path });
+  res.render("shop/add-product", {
+    title: "Add Product",
+    path: req.path,
+    editing: false,
+  });
 };
-exports.postAddProduct = (req,res,next)=>{
-    const title = req.body.title
-    const price = req.body.price
-    Product.create({
-        title,
-        price
-    }).then((result) => {
-        console.log('users created');
-        res.redirect('/')
-    }).catch((err) => {
-        console.log(err);
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const price = req.body.price;
+  Product.create({
+    title,
+    price,
+  })
+    .then((result) => {
+      console.log("users created");
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
     });
-}
+};
--- routes/shop.js
+++ routes/shop.js
@@ -3,4 +3,6 @@ const routes = express.Router()
 const shopControllers = require("../controllers/shop")
 routes.get('/add-products', shopControllers.getAddProduct)
 routes.post('/add-products' , shopControllers.postAddProduct)
+
+
 module.exports = routes
