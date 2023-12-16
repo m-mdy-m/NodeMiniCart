66503bf29933c2c547cc3f856a925f26647d0ca0
mahdi <mahdimamashli1383@gmail.com>
2023-12-16 16:02:40 +0330

    create base project and run server prot 3000

---
+++ app.js
@@ -0,0 +1,27 @@
+// Require the necessary packages
+const bodyParser = require("body-parser")
+const express = require("express")
+const path = require('path')
+
+// Create an express app
+const app = express()
+
+// Middleware for parsing incoming requests with URL-encoded payloads
+app.use(bodyParser.urlencoded({extended : false}))
+
+// Set the view engine to ejs and the views directory
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+// Set the static directory for serving static files like images, stylesheets, and scripts
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Create a route for the home page
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+// Start the server and listen on port 3000
+app.listen(3000, ()=>{
+    console.log('server run on 3000');
+})
