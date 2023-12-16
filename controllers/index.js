dbc805416bb8c87f3fcab96725b89874c1c9d3d3
mahdi <mahdimamashli1383@gmail.com>
2023-12-16 17:02:35 +0330

    Add products to the home page

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,11 @@
+const Product = require("../models/products")
 exports.getHome = (req,res,next)=>{
-    res.render('index', {
-        title : 'HOME',
-        path : req.path,
+    Product.findAll().then(products =>{
+        res.render('index', {
+            title : 'HOME',
+            path : req.path,
+            products
+        })
     })
+    
 }
