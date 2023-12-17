8021eea4eb40de79d774774c3e7c4c88693990ef
mahdi <mahdimamashli1383@gmail.com>
2023-12-17 12:31:01 +0330

    Refactor admin controller to use async/await

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,62 +1,55 @@
 const Products = require("../models/products");
 
-exports.getAdmin = (req, res, next) => {
-  req.user.getProducts()
-    .then((products) => {
-      res.render("admin/dashboard", {
-        title: "dashboard",
-        path: req.path,
-        products,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+exports.getAdmin = async (req, res, next) => {
+  const products = await req.user.getProducts();
+  await res.render("admin/dashboard", {
+    title: "dashboard",
+    path: req.path,
+    products,
+  });
 };
 
-exports.getEdit = (req, res) => {
+exports.getEdit = async (req, res) => {
   const editMode = req.query.edit;
   let prodsId = req.params.prodsId;
   if (!editMode) {
     return res.redirect("/");
   }
-  req.user.getProducts({ WHERE: { id: prodsId } })
-  .then((products) => {
-    const prods = products[0];
-    res.render("shop/add-product", {
+  try {
+    const user = await req.user;
+    console.log("user =>", user);
+    const products = await user.getProducts({ WHERE: { id: prodsId } });
+    const prods = await products[0];
+    await res.render("shop/add-product", {
       title: "editUser",
       path: req.path,
       editing: editMode,
       prods,
     });
-  });
+  } catch (err) {
+    console.log(err);
+  }
 };
-exports.editProducts = (req, res) => {
+exports.editProducts = async (req, res) => {
   const body = req.body;
-  const prodsId = body.id;
+  const id = body.id;
   const title = body.title;
   const price = body.price;
-
-  Products.findByPk(prodsId)
-    .then((pro) => {
-      pro.title = title;
-      pro.price = price;
-      return pro.save();
-    })
-    .then((result) => {
-      console.log("user edit");
-      res.redirect("/");
-    });
+  try {
+    const product = await Products.findByPk(id);
+    product.title = title;
+    product.price = price;
+    await product.save();
+    console.log("user edit");
+    res.redirect("/");
+  } catch (err) {
+    res.redirect(err);
+  }
 };
 
-exports.delete = (req, res) => {
+exports.delete = async (req, res) => {
   const id = req.params.prodsId;
-  Products.findByPk(id)
-    .then((prods) => {
-      return prods.destroy();
-    })
-    .then((result) => {
-      console.log("user delete");
-      res.redirect("/admin/dashboard");
-    });
+  const prods = await Products.findByPk(id);
+  await prods.destroy();
+  res.redirect("/admin/dashboard");
 };
