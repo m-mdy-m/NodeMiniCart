50a1d85b338d50c7299db9630291e1d8a3cc525e
mahdi <mahdimamashli1383@gmail.com>
2023-12-17 12:32:32 +0330

    Changed the findAll method to async/await.

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,9 @@
-const Product = require("../models/products")
-exports.getHome = (req,res,next)=>{
-    Product.findAll().then(products =>{
-        res.render('index', {
-            title : 'HOME',
-            path : req.path,
-            products : products
-        })
-    })
-    
-}
+const Product = require("../models/products");
+exports.getHome = async (req, res, next) => {
+  const products = await Product.findAll();
+  res.render("index", {
+    title: "HOME",
+    path: req.path,
+    products: products,
+  });
+};
